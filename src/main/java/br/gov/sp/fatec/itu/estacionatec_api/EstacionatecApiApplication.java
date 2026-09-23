d0f87c61372f3675b39a05dd1bb9f87aab42eaa6
ghgennari <ghgennari@gmail.com>
2026-09-23 14:51:07 -0300

    build(api): configura Spring Boot com Java 21 e banco H2

---
+++ src/main/java/br/gov/sp/fatec/itu/estacionatec_api/EstacionatecApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.itu.estacionatec_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionatecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionatecApiApplication.class, args);
+	}
+
+}
